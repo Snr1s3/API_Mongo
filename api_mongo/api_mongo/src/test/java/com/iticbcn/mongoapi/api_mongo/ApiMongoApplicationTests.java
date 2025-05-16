e4c4b367e6f1df881100329a7a24162090f804dc
Alba Segura <m1ss.sunrse@gmail.com>
2025-05-16 16:58:33 +0200

    update

---
+++ api_mongo/api_mongo/src/test/java/com/iticbcn/mongoapi/api_mongo/ApiMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iticbcn.mongoapi.api_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
